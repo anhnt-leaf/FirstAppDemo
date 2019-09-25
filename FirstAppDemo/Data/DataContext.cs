f0ab4f1829ecad31a6c2862535a7cc00c75a0150
anhnt <anhnt@leaftech.vn>
2019-09-25 09:42:51 +0700

    Initial Commit

---
+++ FirstAppDemo/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using FirstAppDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAppDemo.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ FirstAppDemo/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace FirstAppDemo.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
